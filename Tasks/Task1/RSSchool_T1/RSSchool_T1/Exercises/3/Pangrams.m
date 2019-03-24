f8432dc021847731e7592703814c692df4d4433f
Dzmitry Paplauski <dmitrypoplavskij@gmail.com>
2019-03-25 02:36:15 +0300

    finish pangrams

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
@@ -1,10 +1,35 @@
 #import "Pangrams.h"
 
+
+// Pangrams+Extended
+
+@interface NSCharacterSet(Extended)
+
++ (NSCharacterSet *) latinLettersLowercased;
+
+@end
+
+@implementation NSCharacterSet(Extended)
+
++ (NSCharacterSet *)latinLettersLowercased {
+    return [NSCharacterSet characterSetWithCharactersInString: @"abcdefghijklmnopqrstuvwxyz"];
+}
+
+@end
+
+
+// Pangrams
+
 @implementation Pangrams
 
-// Complete the pangrams function below.
 - (BOOL)pangrams:(NSString *)string {
-    return NO;
+    [string retain];
+    
+    NSString *noWhitespacesLowercasedString = [[string stringByReplacingOccurrencesOfString:@" " withString:@""] lowercaseString];
+    NSCharacterSet *givenSet = [NSCharacterSet characterSetWithCharactersInString:noWhitespacesLowercasedString];
+    
+    [string release];
+    return [givenSet isEqual:NSCharacterSet.latinLettersLowercased];
 }
 
 @end
