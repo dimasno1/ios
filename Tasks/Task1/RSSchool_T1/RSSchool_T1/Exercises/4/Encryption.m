fdfd5e24da71c49011915774047434ee974efc03
Dzmitry Paplauski <dmitrypoplavskij@gmail.com>
2019-03-25 23:09:41 +0300

    code refactoring

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
@@ -5,19 +5,19 @@
 // Complete the encryption function below.
 - (NSString *)encryption:(NSString *)string {
     [string retain];
-    
+
     NSString *noWhitespacesMessage = [string stringByReplacingOccurrencesOfString:@" " withString:@""];
     NSUInteger lenght = [noWhitespacesMessage length];
     NSNumber *lenghtSquareRoot = @(sqrt(lenght));
-    
+
     int rows = floorf(lenghtSquareRoot.doubleValue);
     int columns = ceilf(lenghtSquareRoot.doubleValue);
     int lastRowLettersCount = (lenght % columns) == 0 ? columns : lenght % columns;
-    
+
     if (rows * columns < lenght) {
         rows < columns ? rows++ : columns++;
     }
-    
+
     NSMutableArray *array = [NSMutableArray arrayWithCapacity:rows];
     for (int i = 0; i < rows; i++) {
         BOOL lastRow = i == rows - 1;
@@ -25,31 +25,30 @@
         NSString *cuttedMessage = [noWhitespacesMessage substringWithRange:range];
         [array addObject:cuttedMessage];
     }
-    
+
     [string release];
-    return [self encryptedStringWithContentsOfArray:array];
+    return [self encryptedStringFromArray:array];
 }
 
-- (NSString *)encryptedStringWithContentsOfArray:(NSArray *)array {
+- (NSString *)encryptedStringFromArray:(NSArray *)array {
     [array retain];
     NSInteger capacity = [[array firstObject] length];
-    NSMutableArray *encrypted = [NSMutableArray arrayWithCapacity:capacity];
-    
+    NSMutableArray<NSMutableString *> *encrypted = [NSMutableArray arrayWithCapacity:capacity];
+
     for (int i = 0; i < capacity; i++) {
-        [encrypted addObject:@""];
+        NSMutableString *emptyString = [[NSMutableString new] autorelease];
+        [encrypted addObject:emptyString];
     }
-    
+
     for (int rowIndex = 0; rowIndex < [array count]; rowIndex++) {
         NSString *cuttedString = [array objectAtIndex:rowIndex];
+        
         for(int letterIndex = 0; letterIndex < [cuttedString length]; letterIndex ++) {
-            NSString *character = [NSString stringWithFormat:@"%C", [cuttedString characterAtIndex:letterIndex]];
-            NSMutableString *resultString = [[[encrypted objectAtIndex:letterIndex] mutableCopy] autorelease];
-            
-            [resultString appendString:character];
-            [encrypted replaceObjectAtIndex:letterIndex withObject:resultString];
+            char character = [cuttedString characterAtIndex:letterIndex];
+            [[encrypted objectAtIndex:letterIndex] appendString:[NSString stringWithFormat:@"%c", character]];
         }
     }
-    
+
     [array release];
     return [encrypted componentsJoinedByString:@" "];
 }
